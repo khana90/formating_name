1719af10323ba686558e11e3850806eae056e7df
A khan <awal.khan@live.co.uk>
2024-07-25 23:08:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "you didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+
+  return f"result: {formated_f_name} {formated_l_name}"
+
+print(format_name(input("whats your first name? "),input("whats your last name ? ")))
